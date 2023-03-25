10b24164fdc14f2164bfb4fe9fccb44ac7d461b9
Gaurav kumar <ergauravkumar02071998@gmail.com>
2023-03-25 11:47:06 +0530

    Delete_Route_Added

--- Backend/src/Routes/TaskRoutes/TaskRoutes.js
+++ Backend/src/Routes/TaskRoutes/TaskRoutes.js
@@ -45,6 +45,32 @@ return res.send(e)
 }
 })
 
+TaskRoute.post("/updateassigne",async(req,res)=>{
+    const {id,email} = req.body;
+    if(!id||!email){
+        return res.send("fiels required")
+    }
+    
+    try{
+          const update= await Tasks.findByIdAndUpdate({_id:id},{email:email})
+          return res.send({mes:"Update",update:update})
+    }catch(e){
+    return res.send(e)
+    }
+    })
+
+    TaskRoute.delete("/delete",async(req,res)=>{
+        const {id } = req.body;
+        if(!id ){
+            return res.send("fiels required")
+        } 
+        try{
+              const update= await Tasks.findByIdAndDelete({_id:id})
+              return res.send({mes:"Deleted",Deleted:update})
+        }catch(e){
+        return res.send(e)
+        }
+        })
 
 
 module.exports=TaskRoute
